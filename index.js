56500bdbbcf6559b98548455d92505112b990fac
jowstafford <91708768+jowstafford@users.noreply.github.com>
2022-06-05 12:50:11 -0500

    finished writin gfunction. forgot that it needed to require file system

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var inquirer = require("inquirer");
 var selectedGroup = [];
 var colleagues = "";
 var executeTemplate = require("./dist/profileTemplate");
+var fs = require("fs");
 var Intern = require("./lib/Intern");
 var Engineer = require("./lib/Engineer");
 var Manager = require("./lib/Manager");
@@ -92,6 +93,34 @@ var addColleague = () => {
         });
     });
 };
+
+function runTemplate(colleagues) {
+  var template = `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <link rel="stylesheet" href="../dist/style.css">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Team Profile Generator</title>
+    </head>
+  <body>
+    <nav class="rosterNavbar">
+      <div class="h1">Listed Roster</div>
+    </nav>
+  <div class="colleagueContainer">
+    <div class="containerRow">
+      ${colleagues}
+    </div>
+  </body>
+</html>
+`;
+
+  fs.writeFile("./src/profile.html", template, (err) => {
+    if (err) {console.log(err);}});
+  return template;
+}
 
 function initialize() {
   runTemplate();
