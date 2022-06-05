102b4f07f899f87cf6295cc15635705d0f3f0143
jowstafford <91708768+jowstafford@users.noreply.github.com>
2022-06-05 16:46:24 -0500

    finished the Employee.test.js by adding four more describe functions that regard the remaining variables.

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -23,4 +23,36 @@ describe("Employee", () => {
     var i = new Employee("Daniel", 1, emailTest);
     expect(i.email).toBe(emailTest);
   });
+
+  describe("nameReturn", () => {
+    it("retrieves full name from nameReturn()", () => {
+      var nameTest2 = "Cruise";
+      var i = new Employee(nameTest2);
+      expect(i.nameReturn()).toBe(nameTest2);
+    });
+  });
+
+  describe("IDReturn", () => {
+    it("retrieves id from IDReturn()", () => {
+      var IDTest2 = 400;
+      var i = new Employee("Justin", IDTest2);
+      expect(i.IDReturn()).toBe(IDTest2);
+    });
+  });
+
+  describe("emailReturn", () => {
+    it("retrieves email address from emailReturn", () => {
+      var emailTest = "ewalker@email.com";
+      var i = new Employee("Emily", 2, emailTest);
+      expect(i.emailReturn()).toBe(emailTest);
+    });
+  });
+
+  describe("roleReturn", () => {
+    it(' returns "Employee" occupation', () => {
+      var employeeTest = "Employee";
+      var i = new Employee("Amanda", 3, "arogue@email.com");
+      expect(i.roleReturn()).toBe(employeeTest);
+    });
+  });
 });
